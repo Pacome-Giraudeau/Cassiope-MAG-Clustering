6ccb3d718363a124382bf6b1999695ab341a7a16
Pacôme GIRAUDEAU <pacome.giraudeau@telecom-sudparis.eu>
2025-04-25 21:49:47 +0200

    augmentation capacités 300 GB et 32 coeurs

--- job_clingo.sh
+++ job_clingo.sh
@@ -2,15 +2,16 @@
 #SBATCH -t 24:00:00 # temps demandé pour le travail (format HH:MM:SS ou J-HH:MM:SS)
 #SBATCH -N 1 # nombre de noeuds demandés
 #SBATCH -n 1 # nombre de tâches à exécuter
-#SBATCH -c 8 # nombre de coeurs (cpus) 
-#SBATCH --mem=50G # mémoire demandée (default: M)
+#SBATCH -c 32 # nombre de coeurs (cpus) 
+#SBATCH --mem=300G # mémoire demandée (default: M)
 #SBATCH -p normal # nom de la partition ("normal", "xlarge" ou "xxlarge")
 #SBATCH --qos=default # nom de la queue de travail. pour normal, choisir default ou long, pour (x)xlarge, (x)xlarge ou (x)xlarge_month -sans parenthèse...)
 #SBATCH --job-name=clingo_1 # nom du job
 #SBATCH --mail-user=pgiraude@genoscope.cns.fr # email utilisateur: à remplir
 #SBATCH --mail-type=BEGIN,FAIL,END #quand envoyer un mail (BEGIN, FAIL, END, ALL)
-#SBATCH --error=log/test.err #chemin du fichier où écrire l'erreur standard
-#SBATCH --output=log/test.out #chemin du fichier où écrire la sortie standard (facultatif, surtout si vous redirigez vous-même la sortie de clingo dans un fichier de sortie)
+#SBATCH --error=log/%x-%J-%u.err #chemin du fichier où écrire l'erreur standard
+#SBATCH --output=log/%x-%J-%u.out #chemin du fichier où écrire la sortie standard (facultatif, surtout si vous redirigez vous-même la sortie de clingo dans un fichier de sortie)
+
 
 cd $SLURM_SUBMIT_DIR
 
