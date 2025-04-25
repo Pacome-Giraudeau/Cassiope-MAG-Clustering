12daec5f1eb460d5b79de433c5c09c9903a70b58
Pacôme GIRAUDEAU <pacome.giraudeau@telecom-sudparis.eu>
2025-04-25 14:56:34 +0200

    job_clingo.sh

---
+++ job_clingo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+#SBATCH -t 24:00:00 # temps demandé pour le travail (format HH:MM:SS ou J-HH:MM:SS)
+#SBATCH -N 1 # nombre de noeuds demandés
+#SBATCH -n 1 # nombre de tâches à exécuter
+#SBATCH -c 8 # nombre de coeurs (cpus) 
+#SBATCH --mem=100G # mémoire demandée (default: M)
+#SBATCH -p normal # nom de la partition ("normal", "xlarge" ou "xxlarge")
+#SBATCH --qos=default # nom de la queue de travail. pour normal, choisir default ou long, pour (x)xlarge, (x)xlarge ou (x)xlarge_month -sans parenthèse...)
+#SBATCH --job-name=clingo # nom du job
+#SBATCH --mail-user= <mail_address> # email utilisateur: à remplir
+#SBATCH --mail-type=FAIL,END #quand envoyer un mail (BEGIN, FAIL, END, ALL)
+#SBATCH -D /env/export/bigtmp2/cassiopee/ # chemin du répertoire de travail
+#SBATCH --error=log/%x-%J-%u.err #chemin du fichier où écrire l'erreur standard
+#SBATCH --output=log/%x-%J-%u.out #chemin du fichier où écrire la sortie standard (facultatif, surtout si vous redirigez vous-même la sortie de clingo dans un fichier de sortie)
+
+
+module load conda #charge le module conda
+conda activate clingo #active l'environnement clingo (modifiez pour le nom de votre environnement le cas échéant)
+
+# commande clingo ou python: a vous de compléter !
+python3 main_to_lp.py
+
+# clingo <options> <predicate_files> > <output_file>
