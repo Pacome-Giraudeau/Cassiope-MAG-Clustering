2936eefab1f836b1a964f691e131ec847988582f
Pacôme GIRAUDEAU <pgiraude@inticns.cnrgh.fr>
2025-04-26 09:19:39 +0200

    transit 1000_clusters

--- main_to_lp.py
+++ main_to_lp.py
@@ -9,7 +9,7 @@ import random
 ###### DESCRPITION DES CHEMINS VERS LES DIFF FICHIERS
 
 nb_clusters = 10
-nb_contigs_to_classify = 300
+nb_contigs_to_classify =3000
 
 
 folder = os.path.dirname(os.path.abspath(sys.argv[0]))
